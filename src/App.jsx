521a3bcefadb8eb73a820d03528691bd4e6b2958
ErikBergloef <erik.berglof2@hotmail.se>
2024-12-09 13:50:05 +0100

    route syntax steg 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-
-import './App.css'
+import { Route, Routes, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-    
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
