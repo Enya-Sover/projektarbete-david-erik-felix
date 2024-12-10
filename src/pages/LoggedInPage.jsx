520c7084c48d1ca365bc596a9a4f81474cf1f514
Felix0291 <felix.tapper@gmail.com>
2024-12-10 12:00:13 +0100

    länkade api till loggedInPage

--- src/pages/LoggedInPage.jsx
+++ src/pages/LoggedInPage.jsx
@@ -1,9 +1,11 @@
-
+import { useContext } from "react"
+import { CitatContext } from "../context/CitatContext"
 
 const LoggedInPage = ()=>{
-
+    let { greetings } = useContext(CitatContext)
     return(<>
     <h1>Welcome User!</h1>
+    <p>"{greetings.content}" API error </p>
     
     </>)
 }
