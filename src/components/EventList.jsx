7fdfae98f1f9bdc2d9a8b1c1885cc9aca4caa0af
ErikBergloef <47384098+ErikBergloef@users.noreply.github.com>
2024-12-12 17:48:34 +0100

    Revert "erik"
    This reverts commit fc06aa49bccb0bac5b06b199e6d41b12dcdb3372.

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-import Event from "./Event";
+import Event from "./Event"; 
+
 
 const EventList = ({ title, currentUserData }) => {
- 
+
+  console.log(currentUserData?.events)
   return (
     <div className="event-list">
       <h3>{title}</h3>
-      {events?.length === 0 ? (
+      {/* {currentUserData.events.length === 0 ? (
         <p>No events available.</p>
       ) : (
         <ul>
-          {events?.map((event) => (
+          {currentUserData.events.map((event) => (
             <Event key={event.id} event={event} />
           ))}
         </ul>
-      )}
+      )} */}
+      
     </div>
   );
 };
