e252a20aac3a4c44cfe1534ea8a30d8463f26648
Felix0291 <felix.tapper@gmail.com>
2024-12-19 15:37:06 +0100

    css style

--- src/pages/LoggedInPage.jsx
+++ src/pages/LoggedInPage.jsx
@@ -2,6 +2,7 @@ import { CitatContext } from "../context/CitatContext";
 import { useContext, React } from "react";
 import { LoginContext } from "../context/LoginContext";
 
+
 import { Link, useNavigate } from "react-router-dom";
 
 const LoggedInPage = () => {
@@ -25,18 +26,25 @@ const LoggedInPage = () => {
   const threeEvents = currentUserData.events.filter(event=> new Date(event.start) > today && event.id).sort((a, b) => new Date(a.start) - new Date(b.start) ).slice(-3)
   return (
     <>
-      <header>
-        <h1>Welcome {capitalizeFirstLetter(currentUser)}</h1>
-      </header>
-
-      <nav>
+      <header className="header">
+        <img src="./src/assets/Logo.jpg" alt="logo" className="logo-container"/>
+        <nav>
         <Link to="/loggedin">Start</Link>
         <Link to="/todo">Todos</Link>
         <Link to="/habits">Habits</Link>
         <Link to="/events">Events</Link>
       </nav>
-      <p>Quote of the day: {greetings ? greetings : 'Gandi has spoken'}</p>
+      <div className="btn-container">
+      <button onClick={handleLogOut}>Log out</button>
+      </div>
+      </header>
+      <div className="greeting-container">
+      <h2>Welcome {capitalizeFirstLetter(currentUser)}</h2>
+      <p className="quote">Quote of the day: {greetings ? greetings : 'Gandi has spoken'}</p>
+      </div>
       <main>
+        
+        <div className="todo-container">
       <h2>Your 3 latest added todos:</h2>
 
       <ul>
@@ -46,12 +54,14 @@ const LoggedInPage = () => {
                 <b>Title:</b> {capitalizeFirstLetter(todo.title)}
               </li>
           );
-        }): <p>No todos available</p>}
+        }) : <p>No todos available</p>}
       </ul>
       <p>
-        Click<Link to="/todo"> here </Link>to see all todos
+        See<Link to="/todo"> all </Link>todos
       </p>
+      </div>
 
+      <div className="habits-container">
       <h2>3 Habits with most repetitions:</h2>
       <ul>
         {threeHabits.length > 0 ? (
@@ -61,38 +71,31 @@ const LoggedInPage = () => {
           )) : 
           <p>No habits available</p>}
       </ul>
-      <p>Click<Link to="/habits"> here </Link>to see all habits
+      <p>See<Link to="/habits"> all </Link>habits
       </p>
+      </div>
 
+      <div className="events-container">
       <h2>3 Upcoming events:</h2>
       <ul>
         {threeEvents.length > 0 ? (
           threeEvents.map((event, i) =>
-            event.title !== '' ? <li key={i}> Name: {capitalizeFirstLetter(event.name)}. </li> 
+            event.title !== '' ? <li key={i}>{capitalizeFirstLetter(event.name)} </li> 
           : <p key = {i}>No events available</p>
           )) : 
           <p>No events available</p>}
       </ul>
-      <p>Click<Link to="/events"> here </Link>to see all events
+      <p>See<Link to="/events"> all </Link>events
       </p>
+      </div>
+
       </main>
-      <button onClick={handleLogOut}>Log out</button>
+      
 
-      <footer>
-        <p>Links:</p>
-
-        <ul>
-          <li>
-            {" "}
-            <Link to="/todo">Go to todo page</Link>
-          </li>
-          <li>
-            {" "}
-            <Link to="/events">Event Calendar</Link>
-          </li>
-        </ul>
+      <footer className="footer">
+       <h5>All rights reserved - Copyright 1994</h5>
       </footer>
-      <button><Link to="/habits">Habits</Link></button>
+      
       
     </>
   );
