88231f364b9649fd7bc091d8eec904fdb0cc6c8d
ErikBergloef <erik.berglof2@hotmail.se>
2024-12-20 12:38:39 +0100

    css-login page

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -6,7 +6,7 @@ const LogIn = () => {
   let { regUser, setCurrentUser } = useContext(LoginContext);
   const navigate = useNavigate();
 
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false); 
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
   const [error, setError] = useState("");
   const [loginUsername, setLoginUsername] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
@@ -16,11 +16,11 @@ const LogIn = () => {
       (user) => user.userName === char && user.password === pass
     );
     if (requestUser) {
-      setCurrentUser(requestUser.userName)
-        localStorage.setItem("currentUser", JSON.stringify(requestUser.userName))
+      setCurrentUser(requestUser.userName);
+      localStorage.setItem("currentUser", JSON.stringify(requestUser.userName));
       navigate("/loggedin");
       setIsUserLoggedIn(true);
-      window.location.reload()
+      window.location.reload();
     } else {
       setError("Wrong input!");
     }
@@ -29,17 +29,25 @@ const LogIn = () => {
   return (
     <div className="log-in-container">
       <h2>Log in</h2>
-      <label htmlFor="username">Username: </label>
-      <input
-        type="text"
-        placeholder="Username"
-        onChange={(e) => setLoginUsername(e.target.value)}
-      />
-      <br />
-      <label htmlFor="password">Password: </label>
-      <input type="password" placeholder="Password" onChange={(e) => setLoginPassword(e.target.value)} />
-      <br />
-      <button onClick={() => checkLogin(loginUsername, loginPassword)}>Log in</button>
+      <div className="input-login-container">
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setLoginUsername(e.target.value)}
+        />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setLoginPassword(e.target.value)}
+        />
+        <br />
+      </div>
+      <button onClick={() => checkLogin(loginUsername, loginPassword)}>
+        Log in
+      </button>
       <p>{error}</p>
     </div>
   );
